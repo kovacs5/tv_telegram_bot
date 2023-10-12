6fb8d6540a50e29ad5c7073671c225c08e6a1431
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2023-10-12 14:36:17 +0300

    Update tradingview_crypto_uclu.py

--- tradingview_crypto_uclu.py
+++ tradingview_crypto_uclu.py
@@ -19,7 +19,7 @@ symbol_list_3 = ['TOTAL', 'TOTAL2', 'TOTAL3']
 
 intervals_2 = [Interval.in_daily, Interval.in_4_hour, Interval.in_1_hour]
 
-sembol_listesi = [symbol_list, symbol_list_2]
+sembol_listesi = [symbol_list, symbol_list_2, symbol_list_3]
 
 for interval_1 in intervals_2:
 
@@ -30,7 +30,7 @@ for interval_1 in intervals_2:
             if (semboller == symbol_list_3):
                 exchange = 'CRYPTOCAP'
             else:
-                exchange = 'BINANCE'
+                exchange ='BINANCE'
 
             df = tv.get_hist(symbol=symbol,exchange=exchange,interval=interval_1,n_bars=1000)
             fast = 8
@@ -70,3 +70,56 @@ for interval_1 in intervals_2:
 
             if((macd_son < signal_son and macd_son_1 > signal_son_1) & (rsi < rsi_1)):
                 send_msg('⚡ ' + symbol + ' için ' + interval_text_1 + ' MACD CROSS ve RSI AŞAĞI 🔴\n' + 'Anlık Değer: ' + str(son_kapanis) + '\n' + 'Önceki Değer: ' + str(onceki_kapanis))
+
+
+symbol_list_4 = ['THYAO','TTKOM','SISE','BIENY','MERCN','KLKIM','DZGYO','CRFSA','KONTR','DOAS','ENERY','GESAN','ENKAI','TMSN','ZOREN','KONKA','KOPOL','GSDHO']
+symbol_list_5 = ['RAYSG','YEOTK','KCAER','TRCAS','BERA','CEMAS','METRO','KATMR','ADESE','EUPWR','KLSER','GWIND','GOKNR','SMRTG','HUNER','OYAKC','BRLSM','CANTE']
+
+sembol_listesi_2 = [symbol_list_4, symbol_list_5]
+
+intervals_4 = [Interval.in_daily, Interval.in_4_hour]
+
+for interval_3 in intervals_4:
+
+    for semboller_2 in sembol_listesi_2:
+
+        for symbol2 in semboller_2:
+
+            df2 = tv.get_hist(symbol=symbol2,exchange='BIST',interval=interval_3,n_bars=1000)
+            df2fast = 8
+            df2slow = 16
+            df2signal = 11
+            
+            # Calculate short-term and long-term moving averages
+            df2macd_value = macd(df2['close'],df2slow,df2fast)
+            df2['macd'] = df2macd_value
+            df2signal_value = pandas_ta.sma(df2macd_value,df2signal)
+            df2['signal'] = df2signal_value
+
+            df2son_kapanis = df2['close'].iloc[-1]
+            df2onceki_kapanis = df2['close'].iloc[-2]
+
+            df2macd_son = df2['macd'].iloc[-1]
+            df2macd_son_1 = df2['macd'].iloc[-2]
+            df2signal_son = df2['signal'].iloc[-1]
+            df2signal_son_1 = df2['signal'].iloc[-2]
+
+            df2['rsi'] = RSIIndicator(df2['close'], window=14).rsi()
+
+            df2rsi = df2['rsi'].iloc[-1]
+            df2rsi_1 = df2['rsi'].iloc[-2]
+
+            if interval_3 == Interval.in_daily:
+                interval_text_3 = "günlükte"
+
+            if interval_3 == Interval.in_4_hour:
+                interval_text_3 = "4 saatlikte"
+
+            if interval_3 == Interval.in_1_hour:
+                interval_text_3 = "1 saatlikte"
+
+            if((df2macd_son > df2signal_son and df2macd_son_1 < df2signal_son_1) & (df2rsi > df2rsi_1)):
+                send_msg('⚡ ' + symbol2 + ' için ' + interval_text_3 + ' MACD CROSS ve RSI YUKARI 🟢\n' + 'Anlık Değer: ' + str(df2son_kapanis) + '\n' + 'Önceki Değer: ' + str(df2onceki_kapanis))
+
+            if((df2macd_son < df2signal_son and df2macd_son_1 > df2signal_son_1) & (df2rsi < df2rsi_1)):
+                send_msg('⚡ ' + symbol2 + ' için ' + interval_text_3 + ' MACD CROSS ve RSI AŞAĞI 🔴\n' + 'Anlık Değer: ' + str(df2son_kapanis) + '\n' + 'Önceki Değer: ' + str(df2onceki_kapanis))
